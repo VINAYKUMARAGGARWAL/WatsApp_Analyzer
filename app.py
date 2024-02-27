f978c696e7467457e5b77bea81a5d624ba1535d3
VINAY AGGARWAL <117700164+VINAYKUMARAGGARWAL@users.noreply.github.com>
2024-02-27 19:55:03 +0530

    1app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+ import streamlit as st
 import re
 import pandas as pd
 import numpy as np
@@ -11,15 +11,9 @@ import warnings
 
 warnings.filterwarnings('ignore')
 
-# Magic command for inline plotting
-# %matplotlib inline
-
 # Function to convert raw data to DataFrame
 def raw_to_df(file, key):
     # Function implementation here
-
-# Load data
-df = raw_to_df('edited_chats.txt', '12hr')
 
 # Title
 st.title('WhatsApp Group Chat Analysis')
@@ -31,23 +25,8 @@ analysis_option = st.sidebar.selectbox('Select Analysis', ('Overall Frequency', 
 if analysis_option == 'Overall Frequency':
     # Overall frequency of the group
     st.subheader('Overall Frequency of the Group')
+    # Call raw_to_df function
+    df = raw_to_df('edited_chats.txt', '12hr')
     # Plotting messages sent per day over a time period
     # Replace plt.plot() with Streamlit's native plotting function
     st.pyplot(plot_overall_frequency(df))
-
-# Define functions for plotting and analysis
-def plot_overall_frequency(df):
-    df1 = df.copy()
-    df1['message_count'] = 1
-
-    if 'year' in df1.columns:
-        df1.drop(columns='year', inplace=True)
-
-    df1 = df1.groupby('date')['message_count'].sum().reset_index()
-
-    # Plotting messages sent per day over a time period
-    plt.plot(df1['date'], df1['message_count'])
-    plt.title('Messages sent per day over a time period')
-    plt.xlabel('Date')
-    plt.ylabel('Message Count')
-    return plt.gcf()
